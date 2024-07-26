19787593a5aec264115def304a3ecad5b0738493
olgazhezhel2 <ojejel32@yandex.ru>
2024-07-26 12:49:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+print("1st program")
+print(9**0.5*5)
+print("2nd program")
+print(9.99>9.98 and 1000!=1000.1)
+print("3rd program")
+print(1234%1000//10+5678%1000//10)
+print("4th program")
+print(int(13.42)==(int(42.13*100)%100))
+
+example= "Python"
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1:6:2])
+print(example[1::2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
