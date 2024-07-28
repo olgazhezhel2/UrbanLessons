e985ab1ffd53f93de8a0e21b7f8c73eb0a3aea79
olgazhezhel2 <ojejel32@yandex.ru>
2024-07-28 21:06:39 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,12 @@
+amount='Python'
+amount='12'
+amount='1.5'
+amount=1.5/12
+
+print("Курс:",amount,',''всего задач:',amount,',',"затрачено часов:",amount,",",'среднее время выполнения',amount)
+
+
+
+
+
+
