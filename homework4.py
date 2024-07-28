c8109c78eccf4da468432a5650b2d8cb8f80b497
olgazhezhel2 <ojejel32@yandex.ru>
2024-07-28 23:49:46 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+my_string=input('Время года сейчас: ')
+print('Время года сейчас'.upper())
+print('Время года сейчас'.lower())
+print('Время года сейчас'.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+
+
+
