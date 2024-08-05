745d473e82a3109c01527d012848da88276dbc6e
olgazhezhel2 <ojejel32@yandex.ru>
2024-08-05 19:42:10 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,12 @@
+my_dict = {'Катя': 11,'Аня': 12,'Оля': 13,"Таня": 15}
+print(my_dict)
+print(my_dict.get('Катя'))
+print(my_dict.get('Саша'))
+my_dict.update({'Макс': 55, 'Дима' : 00})
+print(my_dict)
+my_set = {55, 55, 00, 'Макс', 'Макс', int, 'Оля'}
+print(my_set)
+my_set.add('Дима')
+print(my_set)
+my_set.remove('Дима')
+print(my_set)
