ad6ecb7f26f0cc82eeaf25bc148485ba8a9ef84a
olgazhezhel2 <ojejel32@yandex.ru>
2024-08-14 21:46:33 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
 first = 343
 second = 548
 third = 343
-if first == second or first == third or second == third:
-    print(2)
-elif first == second and second == third:
-    print(3)
-elif first != second and first != third and second != third:
+if first != second and first != third and second != third:
     print(0)
+elif first == second or first == third or second == third:
+    print(2)
+elif first == second and second == third and first == third:
+    print(3)
